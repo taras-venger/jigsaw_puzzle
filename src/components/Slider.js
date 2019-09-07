17e4885c7803dc57341f716a0cb1d9819e1d96db
Taras Venger <taras-venger@users.noreply.github.com>
2019-09-07 16:51:31 +0300

    Create Slider.js

---
+++ src/components/Slider.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Slider = props => {
+  return (
+    <div className='slider'>
+      <p>
+        {props.title}: {props.defaultValue}
+      </p>
+      <input
+        type='range'
+        id={props.id}
+        title={props.title}
+        onInput={props.getValue}
+        defaultValue={props.defaultValue}
+        min='2'
+        max='10'
+        step='1'
+      />
+    </div>
+  );
+};
+
+export default Slider;
