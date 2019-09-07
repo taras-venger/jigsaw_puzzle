975c677ba34985c62f360d08cf1d0d5ae5f1ba2d
Taras Venger <taras-venger@users.noreply.github.com>
2019-09-07 16:51:09 +0300

    Create Cell.js

---
+++ src/components/Cell.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Cell = props => {
+  const { cellWidth, cellHeight } = props;
+
+  const style = {
+    boxSizing: 'border-box',
+    display: 'inline-block',
+    width: cellWidth,
+    height: cellHeight
+  };
+
+  const restylePieceOnDrop = piece => {
+    const ps = piece.style;
+    ps.position = 'relative';
+    ps.top = 0;
+    ps.left = 0;
+    ps.border = 'none';
+    return piece;
+  };
+
+  const highlightCell = e => (e.target.style.backgroundColor = '#e0e0e0');
+  const deemphasizeCell = e => (e.target.style.backgroundColor = '');
+  const dragStart = e => e.preventDefault();
+  const dragOver = e => e.preventDefault();
+  const drop = e => {
+    const pieceID = e.dataTransfer.getData('pieceID');
+    const piece = document.getElementById(pieceID);
+    piece && e.target.append(restylePieceOnDrop(piece));
+  };
+
+  return (
+    <div
+      className='cell'
+      style={style}
+      onDragStart={dragStart}
+      onDragEnter={highlightCell}
+      onDragLeave={deemphasizeCell}
+      onDragOver={dragOver}
+      onDrop={drop}
+    ></div>
+  );
+};
+
+export default Cell;
