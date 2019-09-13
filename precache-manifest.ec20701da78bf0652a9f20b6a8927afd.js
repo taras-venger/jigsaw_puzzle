f98a84f045da3ea4f30e7a650c09fcacfeed96bb
Taras Venger <taras-venger@users.noreply.github.com>
2019-09-13 19:25:41 +0300

    Updates

---
+++ precache-manifest.ec20701da78bf0652a9f20b6a8927afd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7169fe081750303f048325d1127b6b0",
+    "url": "/jigsaw_puzzle/index.html"
+  },
+  {
+    "revision": "43437121de38802036b6",
+    "url": "/jigsaw_puzzle/static/css/main.008a8bf4.chunk.css"
+  },
+  {
+    "revision": "507bd07d387fe9d2ce39",
+    "url": "/jigsaw_puzzle/static/js/2.f4b612ca.chunk.js"
+  },
+  {
+    "revision": "43437121de38802036b6",
+    "url": "/jigsaw_puzzle/static/js/main.523ff685.chunk.js"
+  },
+  {
+    "revision": "2204a8edc5269bde474d",
+    "url": "/jigsaw_puzzle/static/js/runtime~main.5496d0b0.js"
+  },
+  {
+    "revision": "0b09609664b05aa90f804fa260093b6f",
+    "url": "/jigsaw_puzzle/static/media/refresh.0b096096.png"
+  }
+]);
--- src/components/Spinner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Spinner = props => {
-  return <div className='spinner' />;
-};
-
-export default Spinner;
