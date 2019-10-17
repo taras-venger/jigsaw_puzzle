3dd721228f6dbdeeb5978be3c752cb00348739fa
Taras Venger <taras-venger@users.noreply.github.com>
2019-10-18 01:10:38 +0300

    Amended 'checkGameOver' function in order to avoid the direct access to DOM

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Cell = props => {
-  const { dataID, cellWidth, cellHeight } = props;
+  const {
+    dataID,
+    cellWidth,
+    cellHeight,
+    incrementCounter,
+    checkGameOver
+  } = props;
 
   const style = {
     boxSizing: 'border-box',
@@ -24,16 +30,24 @@ const Cell = props => {
   const deemphasizeCell = e => (e.target.style.backgroundColor = '');
   const dragStart = e => e.preventDefault();
   const dragOver = e => e.preventDefault();
-  const drop = e => {
+
+  const drop = async e => {
+    // Append piece
     const pieceID = e.dataTransfer.getData('pieceID');
     const piece = document.getElementById(pieceID);
     piece && e.target.append(restylePieceOnDrop(piece));
+    // Increment counter and fix current position if it's correct
+    if (pieceID === e.target.dataset.id) {
+      await incrementCounter();
+      await checkGameOver();
+      piece.draggable = false;
+    }
   };
 
   return (
     <div
       className='cell'
-      data-cell-id={dataID}
+      data-id={dataID}
       style={style}
       onDragStart={dragStart}
       onDragEnter={highlightCell}
@@ -45,8 +59,11 @@ const Cell = props => {
 };
 
 Cell.propTypes = {
+  dataID: PropTypes.number,
   cellWidth: PropTypes.number,
-  cellHeight: PropTypes.number
+  cellHeight: PropTypes.number,
+  incrementCounter: PropTypes.func,
+  checkGameOver: PropTypes.func
 };
 
 export default Cell;
